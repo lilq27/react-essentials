d92150cddc2dbc439ddf4a6d106a9a5a1f33a6f4
lilq27 <lilaye@naver.com>
2025-02-02 22:00:28 +0900

    25 02 02

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,9 +1,9 @@
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
     //children => 리액트에서 제공
 
     return (
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
         </li>
     );
 }
